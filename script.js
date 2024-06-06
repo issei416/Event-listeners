58610efaceb4ae0733065e22aad326b2e7ff4adf
issei416 <mukeshkj2912@gmail.com>
2024-06-06 21:08:21 +0530

    event listeners

---
+++ script.js
@@ -0,0 +1,47 @@
+var btn_click = document.getElementById('click');
+var btn_mousein = document.getElementById('mouseenter');
+var btn_dblclick = document.getElementById('dblclick');
+
+btn_click.addEventListener('click', () => {
+    let countElement = btn_click.querySelector('.badge #click-count');
+    let count = countElement.innerText;
+    count = parseInt(count)+1;
+    countElement.innerText = count;
+    console.log('clicks :'+count);
+    btn_click.style.backgroundColor = 'rgb('+Math.floor(Math.random()*225)+','+Math.floor(Math.random()*225)+','+Math.floor(Math.random()*225)+',0.6)';
+});
+
+btn_mousein.addEventListener('mouseenter', () => {
+    let countElement1 = btn_mousein.querySelector('.badge #mouse-count');
+    let count1 = countElement1.innerText;
+    count1 = parseInt(count1)+1;
+    countElement1.innerText = count1;
+    console.log('mouse enters:'+count1);
+    btn_mousein.style.backgroundColor = 'rgb('+Math.floor(Math.random()*225)+','+Math.floor(Math.random()*225)+','+Math.floor(Math.random()*225)+',0.6)';
+});
+
+btn_dblclick.addEventListener('dblclick',() => {
+    let countElement2 = btn_dblclick.querySelector('.badge #dblclick-count');
+    let count2 = countElement2.innerText;
+    count2 = parseInt(count2)+1;
+    countElement2.innerText = count2;
+    console.log('double clicks:'+count2);
+    btn_dblclick.style.backgroundColor = 'rgb('+Math.floor(Math.random()*225)+','+Math.floor(Math.random()*225)+','+Math.floor(Math.random()*225)+',0.6)';
+});
+
+let btn_reset = document.querySelector('#reset');
+btn_reset.addEventListener('click' , () => {
+    let click_count = document.querySelector('#click .badge #click-count');
+    click_count.innerText = 0; 
+    btn_click.style.backgroundColor = 'transparent';
+    let mouse_count = document.querySelector('#mouseenter .badge #mouse-count');
+    mouse_count.innerText = 0; 
+    btn_mousein.style.backgroundColor = 'transparent';
+    let db_count = document.querySelector('#dblclick .badge #dblclick-count');
+    db_count.innerText = 0; 
+    btn_dblclick.style.backgroundColor = 'transparent'
+});
+
+
+
+
